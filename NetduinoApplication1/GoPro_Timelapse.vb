e5ddff776bb78a049e444a96961f966febc77007
sethicle <hudson_seth@hotmail.com>
2016-04-19 00:25:09 +1000

    3

--- NetduinoApplication1/GoPro_Timelapse.vb
+++ NetduinoApplication1/GoPro_Timelapse.vb
@@ -1,30 +1,33 @@
+Option Explicit On
 Imports Microsoft.SPOT
 Imports Microsoft.SPOT.Hardware
 Imports SecretLabs.NETMF.Hardware
-Imports SecretLabs.NETMF.Hardware.Netduino
+Imports SecretLabs.NETMF.Hardware.NetduinoPlus
 Imports Toolbox.NETMF.Hardware
 Imports System.Threading
 Imports SecretLabs.NETMF.IO
-Imports System.IO
 
 Module GoPro_TimeLapse
     
-
+    
 
     Sub Main()
         Dim RTC As DS1307 = New DS1307()
-        Dim TimeHour As Double
         Dim TimeSecond As Double = DateTime.Now.Second
-        Dim led = New OutputPort(Pins.ONBOARD_LED, False)
-        Dim CamRelay = New OutputPort(Pins.GPIO_PIN_D5, False)
+        Dim TimeHour As Double
+        'Dim led = New OutputPort(Pins.ONBOARD_LED, False)
+        'Dim CamRelay = New OutputPort(Pins.GPIO_PIN_D5, False)
+        'Dim ShieldLED1 = New OutputPort(Pins.GPIO_PIN_D3, False)
 
-        RTC.SetTime(Year:=2016, Month:=4, Day:=13, Hour:=23, Minute:=6, Second:=30) 'time set to current time. set once at initial install then commented.
+
+        RTC.SetTime(Year:=2016, Month:=4, Day:=18, Hour:=22, Minute:=30, Second:=0) 'time set to current time. set once at initial install then commented.
         RTC.Synchronize()
-
-        While (True)
-            TimeHour = DateTime.Now.Minute
+        Debug.Print(DateTime.Now.ToString())
+        Do While True
+            Photo()
+            TimeHour = DateTime.Now.Hour
             Select Case TimeHour
-                Case 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 23, 24
+                Case 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 22, 23, 0
                     If DateTime.Now.Minute = 0 OrElse DateTime.Now.Minute = 15 OrElse DateTime.Now.Minute = 30 OrElse DateTime.Now.Minute = 45 Then
                         If DateTime.Now.Second <> 0 OrElse DateTime.Now.Second <> 1 OrElse DateTime.Now.Second <> 2 OrElse DateTime.Now.Second <> 3 _
                         OrElse DateTime.Now.Second <> 4 OrElse DateTime.Now.Second <> 5 Then
@@ -35,7 +38,7 @@ Module GoPro_TimeLapse
 
                     End If
             End Select
-        End While
+        Loop
 
 
         ' If DateTime.Now.Hour = 23 Then
@@ -43,7 +46,7 @@ Module GoPro_TimeLapse
         'End If
 
     End Sub
-    Sub Photo()
+    Private Sub Photo()
         Dim RTC As DS1307 = New DS1307()
         Dim TimeSecond As Double = DateTime.Now.Second
         Dim led = New OutputPort(Pins.ONBOARD_LED, False)
@@ -65,6 +68,18 @@ Module GoPro_TimeLapse
         lastphoto = DateTime.Now.ToString()
         Debug.Print("The Photo Count is: " & PhotoCount)
         Debug.Print(lastphoto)
+
+    End Sub
+
+    Private Sub NoPhoto()
+        Dim ShieldLED1 = New OutputPort(Pins.GPIO_PIN_D3, False)
+        For A = 1 To 10
+            ShieldLED1.Write(True)
+            Thread.Sleep(500)
+            ShieldLED1.Write(False)
+            Thread.Sleep(500)
+        Next
+
 
     End Sub
 
--- NetduinoApplication1/My Project/AssemblyInfo.vb
+++ NetduinoApplication1/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("NetduinoApplication1")> 
-<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyCopyright("Seth Hudson - Copyright ©  2016")> 
 <Assembly: AssemblyTrademark("")> 
 
 ' Version information for an assembly consists of the following four values:
@@ -23,5 +23,5 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.0.1")> 
+<Assembly: AssemblyFileVersion("1.0.0.1")> 
